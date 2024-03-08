833ca3d3b4e430f6c1dccadc12f74274be7da19b
Pedro Nunes <pedronunes.dev@gmail.com>
2024-03-08 16:19:55 +0000

    feat: Create express server

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import OpenAI from "openai";
+import express from "express";
 
 const openai = new OpenAI({
   organization: 'org-2G540qGtxrMq5HX1cOMBuN33',
   apiKey: process.env.OPENAI_API_KEY
 });
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a great software developer assistant." }],
-    model: "gpt-3.5-turbo",
+const app = express();
+const port = 4000;
 
-    // temperature: 1,
-    // max_tokens: 7
-  });
+app.listen(port, () => {
+    console.log(`Server running in port ${port}`)
+})
 
-  console.log(completion.choices[0]);
-}
-
-main();
+app.post('/', async (req, res) => {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: "You are a great software developer assistant." }],
+        model: "gpt-3.5-turbo",
+    
+        // temperature: 1,
+        // max_tokens: 7
+      });
+    
+    console.log(completion.choices[0]);
+    res.json({ data: response.data })
+})
