b8829de4c9cf6dd91fb60ef4681fa4c254757aff
Pedro Nunes <pedronunes.dev@gmail.com>
2024-03-08 10:00:01 +0000

    feat: style the chat message interface

--- chatgpt-pg-client/src/App.js
+++ chatgpt-pg-client/src/App.js
@@ -11,6 +11,20 @@ function App() {
         </div>
       </aside>
       <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+              <div className="avatar"></div>
+              <div className="message">text s</div>
+            </div>
+          </div>
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+              <div className="avatar chatgpt"></div>
+              <div className="message">I am an AI</div>
+            </div>
+          </div>
+        </div>
         <div className="chat-input-holder">
           <textarea 
             rows="1"
